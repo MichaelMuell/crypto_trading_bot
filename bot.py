ffcceabf0be806471bfa7782323d08b57f877522
Michael Mll <michaelmuell95@gmail.com>
2021-05-28 16:08:26 +0200

    Receiving closing data of candle stick captured in numpy array

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ import websocket
 import binance
 
 SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
-
+closes = []
 
 def on_open(ws):
     print("connection opened")
@@ -18,8 +18,18 @@ def on_close(ws):
 
 
 def on_message(ws, message):
-    print("received message")
-    print(message)
+    global closes
+
+    json_message = json.loads(message)
+    candle = json_message['k']
+    is_candle_closed = candle['x']
+    pprint.pprint(is_candle_closed)
+    candle_closed = candle['c']
+
+    if is_candle_closed:
+        pprint.pprint(candle_closed)
+        closes.append(float(candle_closed))
+        print(closes)
 
 ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
 ws.run_forever()
